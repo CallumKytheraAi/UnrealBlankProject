7df7fd65d164cc0d0c54472076752af47bf8b14e
Callum Morrison <callumm@kythera.ai>
2023-12-18 15:38:52 +0000

    Initial commit for Unreal5_3 source

--- Source/UnrealBlankProject.Target.cs
+++ Source/UnrealBlankProject.Target.cs
@@ -5,12 +5,11 @@ using System.Collections.Generic;
 
 public class UnrealBlankProjectTarget : TargetRules
 {
-	public UnrealBlankProjectTarget( TargetInfo Target) : base(Target)
+	public UnrealBlankProjectTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("UnrealBlankProject");
-		CppStandard = CppStandardVersion.Default;
 	}
 }
