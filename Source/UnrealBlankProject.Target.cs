445c1bac660fa0f4944332f366af0f4b702d9863
Callum Morrison <callumm@kythera.ai>
2023-12-18 11:58:51 +0000

    Source 5.2 initial commit

--- Source/UnrealBlankProject.Target.cs
+++ Source/UnrealBlankProject.Target.cs
@@ -8,11 +8,8 @@ public class UnrealBlankProjectTarget : TargetRules
 	public UnrealBlankProjectTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.Latest;
-		ExtraModuleNames.AddRange( new string[] { "UnrealBlankProject" } );
-		bLegacyParentIncludePaths = false;
-        CppStandard = CppStandardVersion.Default;
-        WindowsPlatform.bStrictConformanceMode = true;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		ExtraModuleNames.Add("UnrealBlankProject");
 	}
 }
